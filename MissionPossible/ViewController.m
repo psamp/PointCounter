6094fd632bf33278fc798a86514d63800b79cb0e
Princess Sampson <ps@kingonomics.com>
2016-08-24 19:28:46 -0400

    Will complete one and two star challenges

--- MissionPossible/ViewController.m
+++ MissionPossible/ViewController.m
@@ -9,11 +9,30 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-- (IBAction)leftButton:(UIButton *)sender;
+
+@property (strong, readwrite, nonatomic) IBOutlet UIButton *leftButton;
+@property (strong, readwrite, nonatomic) IBOutlet UIButton *rightButton;
+@property(nonatomic, readwrite) NSInteger leftCounter;
+@property(nonatomic, readwrite) NSInteger rightCounter;
+
+- (IBAction)updateLeftButton;
+- (IBAction)updateRightButton;
+- (IBAction)resetAllButtons;
+- (IBAction)updateButton:(UIButton *)sender
+               withCount:(NSInteger)count;
 
 @end
 
 @implementation ViewController
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _leftCounter = 0;
+        _rightCounter = 0;
+    }
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -26,11 +45,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)updateLeftButton {
+    self.leftCounter++;
+    [self updateButton:self.leftButton withCount:self.leftCounter];
+}
 
-- (IBAction)leftButton:(UIButton *)sender {
-    NSInteger currentCount = [sender.currentTitle integerValue];
-    currentCount++;
+- (IBAction)updateRightButton {
+    self.rightCounter++;
+    [self updateButton:self.rightButton withCount:self.rightCounter];
+
+}
+
+- (IBAction)updateButton:(UIButton *)sender
+               withCount:(NSInteger)count {
     
-    [sender setTitle:[NSString stringWithFormat:@"%@", @(currentCount)] forState:UIControlStateNormal];
+    [sender setTitle:[NSString stringWithFormat:@"%@", @(count)] forState:UIControlStateNormal];
+}
+
+- (IBAction)resetAllButtons {
+    self.leftCounter = -1;
+    self.rightCounter = -1;
+  
+    [self updateLeftButton];
+    [self updateRightButton];
+    
 }
 @end
