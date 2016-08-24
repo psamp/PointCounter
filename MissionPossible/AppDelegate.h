024d03e5a1f1e196ba6b66f0c6c041d3a6cb8892
Princess Sampson <ps@kingonomics.com>
2016-08-24 15:25:09 -0400

    Initial Commit

---
+++ MissionPossible/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MissionPossible
+//
+//  Created by Princess Sampson on 8/24/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MissionPossible/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MissionPossible
+//
+//  Created by Princess Sampson on 8/24/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
